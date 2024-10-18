757781e8247a4915459ffcbea73ae7e8f43decdf
arm137039 <la227039@student.helha.be>
2024-10-19 00:34:17 +0200

    commit des changement de charles et armand

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,35 +1,35 @@
 import { RequestHandler } from 'express';
+import { AppDataSource } from '../data-source';
+import { User } from '../entity/User';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import User from '../models/User';
 
 class UserController {
     static register: RequestHandler = async (req, res, next) => {
         try {
             const { email, password } = req.body;
+            const userRepository = AppDataSource.getRepository(User);
 
-            // Vérifier si l'utilisateur existe déjà
-            const existingUser = await User.findOne({ where: { email } });
+            const existingUser = await userRepository.findOneBy({ email });
             if (existingUser) {
                 res.status(400).json({ message: 'Cet e-mail est déjà utilisé.' });
                 return;
             }
 
-            // Hasher le mot de passe
             const hashedPassword = await bcrypt.hash(password, 10);
 
-            // Créer l'utilisateur
-            const user = await User.create({ email, password: hashedPassword });
+            const user = new User();
+            user.email = email;
+            user.password = hashedPassword;
 
-            // Générer un token JWT
+            await userRepository.save(user);
+
             const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
 
             res.json({ token });
-            return;
         } catch (error) {
             console.error('Erreur lors de la création du compte :', error);
             res.status(500).json({ message: 'Erreur serveur. register' });
-            return;
         }
     };
 
@@ -38,37 +38,32 @@ class UserController {
             const { email, password } = req.body;
 
             // provisoire
-            if (email === 'admin@email' && password === 'password') {
-                // Générer un token JWT avec des informations spécifiques pour l'admin
+            if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
                 const token = jwt.sign({ role: 'admin' }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
                 res.json({ token });
                 return;
             }
 
-            // Vérifier si l'utilisateur existe
-            const user = await User.findOne({ where: { email } });
+            const userRepository = AppDataSource.getRepository(User);
+
+            const user = await userRepository.findOneBy({ email });
             if (!user) {
                 res.status(400).json({ message: 'Email ou mot de passe incorrect.' });
                 return;
             }
 
-            // Comparer le mot de passe
             const isMatch = await bcrypt.compare(password, user.password);
             if (!isMatch) {
                 res.status(400).json({ message: 'Email ou mot de passe incorrect.' });
                 return;
             }
 
-            // Générer un token JWT
             const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
-
-            // Envoyer le token au client
             res.json({ token });
-            return;
+
         } catch (error) {
             console.error('Erreur lors de la connexion :', error);
-            res.status(500).json({ message: 'Erreur serveur. login' });
-            return;
+            res.status(500).json({ message: 'Erreur serveur. login' });;
         }
     };
 }
